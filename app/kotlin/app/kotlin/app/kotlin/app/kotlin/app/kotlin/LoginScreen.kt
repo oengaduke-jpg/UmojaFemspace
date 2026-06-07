d0d477d45dfdd8e71abe5dd4e2c607b1f16155fe
oengaduke-jpg <oengaduke@gmail.com>
2026-06-07 13:01:09 +0300

    Add Login Screen Model

---
+++ app/kotlin/app/kotlin/app/kotlin/app/kotlin/app/kotlin/LoginScreen.kt
@@ -0,0 +1,8 @@
+package com.umojafemspace.app
+
+data class LoginScreen(
+    val title: String = "Umoja Femspace",
+    val slogan: String = "Start • Scale • Succeed",
+    val phoneNumber: String = "",
+    val password: String = ""
+)
