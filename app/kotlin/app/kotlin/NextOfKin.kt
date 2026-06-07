2c859960e321789d514b098f1f1edca282786875
oengaduke-jpg <oengaduke@gmail.com>
2026-06-07 20:27:55 +0300

    Add Next Of Kin Model

---
+++ app/kotlin/app/kotlin/NextOfKin.kt
@@ -0,0 +1,11 @@
+package com.umojafemspace.app
+
+data class NextOfKin(
+    val kinId: String,
+    val clientId: String,
+    val fullName: String,
+    val phoneNumber: String,
+    val nationalId: String,
+    val relationship: String,
+    val address: String
+)
