023bd672aab2e5e1e052591d6d82b27b8c84e436
oengaduke-jpg <oengaduke@gmail.com>
2026-06-07 20:24:08 +0300

    Add Loan Status Enum

---
+++ app/kotlin/app/kotlin/app/kotlin/app/kotlin/app/kotlin/LoanStatus.kt
@@ -0,0 +1,13 @@
+package com.umojafemspace.app
+
+enum class LoanStatus {
+    PENDING,
+    UNDER_REVIEW,
+    APPROVED,
+    REJECTED,
+    DISBURSED,
+    ACTIVE,
+    OVERDUE,
+    CLOSED,
+    DEFAULTED
+}
