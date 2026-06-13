e572e2542de86d7a8a7c5c5c1da09e7e84e31280
oengaduke-jpg <oengaduke@gmail.com>
2026-06-13 10:25:46 +0300

    Add Loan Type Enum

---
+++ app/kotlin/app/kotlin/app/kotlin/app/kotlin/app/kotlin/app/kotlin/app/kotlin/app/kotlin/app/kotlin/LoanType.kt
@@ -0,0 +1,12 @@
+package com.umojafemspace.app
+
+enum class LoanType {
+    BUSINESS,
+    EMERGENCY,
+    WOMEN_ENTERPRISE,
+    YOUTH_ENTERPRISE,
+    AGRICULTURE,
+    SCHOOL_FEES,
+    ASSET_FINANCING,
+    GROUP
+}
