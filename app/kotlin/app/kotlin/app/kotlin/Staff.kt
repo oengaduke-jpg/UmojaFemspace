0b5ff103b5f1d139ab68ef3b2a0fc8d22ec89087
oengaduke-jpg <oengaduke@gmail.com>
2026-06-13 20:23:36 +0300

    Add Staff Model

---
+++ app/kotlin/app/kotlin/app/kotlin/Staff.kt
@@ -0,0 +1,11 @@
+package com.umojafemspace.app
+
+data class Staff(
+    val staffId: String,
+    val fullName: String,
+    val role: String,
+    val phoneNumber: String,
+    val email: String,
+    val employmentStatus: String,
+    val dateJoined: String
+)
