42871c749bc8508757e23a621505a42fdf92bd9a
oengaduke-jpg <oengaduke@gmail.com>
2026-06-14 19:09:16 +0300

    Add Firestore Collections

---
+++ app/firebase/app/firebase/app/auth/app/ui/app/ui/app/firebase/app/firebase/FirestoreCollections.kt
@@ -0,0 +1,11 @@
+package com.umojafemspace.firebase
+
+object FirestoreCollections {
+    const val CLIENTS = "clients"
+    const val LOANS = "loans"
+    const val REPAYMENTS = "repayments"
+    const val GUARANTORS = "guarantors"
+    const val STAFF = "staff"
+    const val BRANCHES = "branches"
+    const val NOTIFICATIONS = "notifications"
+}
