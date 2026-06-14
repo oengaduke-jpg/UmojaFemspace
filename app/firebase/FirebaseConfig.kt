b337741b9fbd5283a7d8188c19f2a455faf93ea6
oengaduke-jpg <oengaduke@gmail.com>
2026-06-14 18:52:57 +0300

    Add Firebase Configuration

---
+++ app/firebase/FirebaseConfig.kt
@@ -0,0 +1,10 @@
+package com.umojafemspace.firebase
+
+object FirebaseConfig {
+    const val CLIENTS_COLLECTION = "clients"
+    const val LOANS_COLLECTION = "loans"
+    const val REPAYMENTS_COLLECTION = "repayments"
+    const val GUARANTORS_COLLECTION = "guarantors"
+    const val STAFF_COLLECTION = "staff"
+    const val NOTIFICATIONS_COLLECTION = "notifications"
+}
