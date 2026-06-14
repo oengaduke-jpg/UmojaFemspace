b9b4524681a237372e7102646a8b70fede2bcd93
oengaduke-jpg <oengaduke@gmail.com>
2026-06-14 09:08:34 +0300

    Add App Configuration

---
+++ app/kotlin/app/kotlin/app/kotlin/app/kotlin/app/kotlin/app/kotlin/AppConfig.kt
@@ -0,0 +1,9 @@
+package com.umojafemspace.app
+
+object AppConfig {
+    const val COMPANY_NAME = "Umoja Femspace"
+    const val SLOGAN = "Start • Scale • Succeed"
+    const val MIN_LOAN = 2000.0
+    const val MAX_LOAN = 100000.0
+    const val INTEREST_RATE = 20.0
+}
