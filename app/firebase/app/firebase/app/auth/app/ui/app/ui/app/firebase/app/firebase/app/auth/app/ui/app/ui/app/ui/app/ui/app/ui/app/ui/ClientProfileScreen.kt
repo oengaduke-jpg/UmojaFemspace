877a3424f228677eb0d04f5a9da27d12ad4951d1
oengaduke-jpg <oengaduke@gmail.com>
2026-06-14 19:18:01 +0300

    Add Client Profile Screen Model

---
+++ app/firebase/app/firebase/app/auth/app/ui/app/ui/app/firebase/app/firebase/app/auth/app/ui/app/ui/app/ui/app/ui/app/ui/app/ui/ClientProfileScreen.kt
@@ -0,0 +1,8 @@
+package com.umojafemspace.ui
+
+data class ClientProfileScreen(
+    val fullName: String,
+    val nationalId: String,
+    val phoneNumber: String,
+    val businessName: String
+)
