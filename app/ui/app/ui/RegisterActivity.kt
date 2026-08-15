b244c215bf7f85562c2f10cbbd5fda589ffd40b3
oengaduke-jpg <oengaduke@gmail.com>
2026-08-15 16:59:55 +0300

    Create RegisterActivity.kt

---
+++ app/ui/app/ui/RegisterActivity.kt
@@ -0,0 +1,8 @@
+package com.umojafemspace.ui
+
+data class RegisterActivity(
+    val fullName: String = "",
+    val nationalId: String = "",
+    val phoneNumber: String = "",
+    val password: String = ""
+)
