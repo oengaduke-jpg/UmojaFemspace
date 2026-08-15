6a2390ee2ae780dacdcbcdc3e861911a004ba4cf
oengaduke-jpg <oengaduke@gmail.com>
2026-08-15 17:10:38 +0300

    Create GuarantorRepository.kt

---
+++ app/ui/app/ui/app/ui/app/ui/app/ui/app/ui/app/ui/app/ui/app/ui/app/ui/app/ui/app/ui/app/ui/app/ui/app/firebase/GuarantorRepository.kt
@@ -0,0 +1,7 @@
+package com.umojafemspace.firebase
+
+class GuarantorRepository {
+    fun saveGuarantor() {}
+    fun getGuarantor(guarantorId: String) {}
+    fun updateVerification(guarantorId: String) {}
+}
