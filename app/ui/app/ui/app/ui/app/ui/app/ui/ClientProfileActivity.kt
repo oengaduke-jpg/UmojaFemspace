f0bc1a9a36813722e59571c941738941b4f0299d
oengaduke-jpg <oengaduke@gmail.com>
2026-08-15 17:01:33 +0300

    Create ClientProfileActivity.kt

---
+++ app/ui/app/ui/app/ui/app/ui/app/ui/ClientProfileActivity.kt
@@ -0,0 +1,9 @@
+package com.umojafemspace.ui
+
+data class ClientProfileActivity(
+    val fullName: String,
+    val nationalId: String,
+    val phoneNumber: String,
+    val businessName: String,
+    val monthlyIncome: Double
+)
