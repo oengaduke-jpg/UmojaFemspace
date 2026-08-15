5c19a29d46b5a9b17c6b5c1ab9c0b8aab70ebd3b
oengaduke-jpg <oengaduke@gmail.com>
2026-08-15 17:08:24 +0300

    Create ClientManagementActivity.kt

---
+++ app/ui/app/ui/app/ui/app/ui/app/ui/app/ui/app/ui/app/ui/app/ui/app/ui/app/ui/app/ui/ClientManagementActivity.kt
@@ -0,0 +1,8 @@
+package com.umojafemspace.ui
+
+data class ClientManagementActivity(
+    val clientId: String,
+    val fullName: String,
+    val phoneNumber: String,
+    val accountStatus: String
+)
